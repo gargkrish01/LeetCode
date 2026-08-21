fc1edc90e2ff09ac2f025afc0fd9490ed9e4b144
Krish Garg <krishgarg712@gmail.com>
2026-08-21 21:31:03 +0530

    Time: 1 ms (99.96%), Space: 46.3 MB (68.11%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if(ransomNote.length()>magazine.length()) return false;
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char ch : magazine.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int[] arr = new int[26];
+        for(int i=0;i<magazine.length();i++){
+            arr[magazine.charAt(i)-'a']++;
         }
         for(int i=0;i<ransomNote.length();i++){
-            char c = ransomNote.charAt(i);
-            if(map.containsKey(c) && map.get(c)>0){
-                map.put(c,map.get(c)-1);
-            }
-            else{
+            arr[ransomNote.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]<0){
                 return false;
             }
         }
